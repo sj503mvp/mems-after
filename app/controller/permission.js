415ec5abd1163c7cea5152e8d72fc85cf04acef6
孙鑫海 <sunxinhai@zhaoshang.net>
2024-05-12 21:35:50 +0800

    '权限完善'

--- app/controller/permission.js
+++ app/controller/permission.js
@@ -24,6 +24,97 @@ class PermissionController extends Controller {
             };
         }
     }
+    async changePermission() {
+        const { ctx, app } = this;
+        const { uid, hasNotifyApproval, hasProcessApproval, hasChangePermission } = ctx.request.body;
+        try {
+            if(!uid) {
+                ctx.body = {
+                    code: 500,
+                    msg: '没有id'
+                }
+                return
+            }
+            const sql = 'UPDATE permission SET hasNotifyApproval=?, hasProcessApproval=?, hasChangePermission=? WHERE uid = ?'
+            const values = [
+                hasNotifyApproval  == 'true'? 'hasNotifyApproval' : null,
+                hasProcessApproval == 'true'? 'hasProcessApproval' : null,
+                hasChangePermission  == 'true'? 'hasChangePermission' : null,
+                parseInt(uid),
+            ]
+            const results = await app.mysql.query(sql, values);
+            if (results.affectedRows > 0) {  
+                ctx.body = {  
+                    code: 200,  
+                    msg: '权限更改成功'  
+                };  
+            } else {  
+                ctx.body = {  
+                    code: 500,
+                    msg: '没有找到要更新的用户或没有发生更改'  
+                };  
+            }  
+        }catch(error) {
+            ctx.status = 500;
+            ctx.body = {
+                code: 500,
+                msg: '服务器错误'
+            }
+        }
+    }
+    async getUnrootUser() {
+        const { ctx, app } = this;
+        try {
+            const result = await app.mysql.query(
+                'SELECT * FROM user WHERE isRoot = 0'
+            )
+            ctx.body = {
+                code: 200,
+                data: result,
+            }
+        }catch(error) {
+            ctx.logger.error(error);
+            ctx.body = { 
+                msg: '登录失败,请重试'
+            };
+            ctx.status = 500;
+        }
+    }
+    async toChangePermission() {
+        const { ctx, app } = this;
+        const { uid, type } = ctx.request.body;
+        let sql;
+        let sqlTwo;
+        if(type == 'add') {
+            sql = 'UPDATE user SET isRoot = 1 WHERE uid = ?'
+            sqlTwo = 'UPDATE permission SET hasChangePermission = \'hasChangePermission\' WHERE uid = ? '
+        }
+        if(type == 'delete') {
+            sql = 'UPDATE user SET isRoot = 0 WHERE uid = ?'
+            sqlTwo = 'UPDATE permission SET hasChangePermission = NULL WHERE uid = ? '
+        }
+        try {
+            const result = await app.mysql.query(sql, [uid]);
+            const resultTwo = await app.mysql.query(sqlTwo, [uid]);
+            if (result.affectedRows > 0 && resultTwo.affectedRows > 0) {  
+                ctx.body = {  
+                    code: 200,  
+                    msg: '权限更改成功'  
+                };  
+            } else {  
+                ctx.body = {  
+                    code: 500,
+                    msg: '修改失败'  
+                };  
+            }  
+        }catch(error) {
+            ctx.logger.error(error);
+            ctx.body = { 
+                msg: '服务器错误'
+            };
+            ctx.status = 500;
+        }
+    }
 }
 
 
--- app/router.js
+++ app/router.js
@@ -10,12 +10,18 @@ module.exports = app => {
   router.post('/register', controller.login.register);
   // 获得权限
   router.get('/getPowerData', controller.permission.getPermission);
+  // 修改权限
+  router.post('/changePowerData', controller.permission.changePermission);
+  // 获得所有非管理员的名单
+  router.post('/getUnrootUser', controller.permission.getUnrootUser);
+  // 修改权限的权限
+  router.post('/changePermission', controller.permission.toChangePermission)
   // 获取登录用户信息
   router.get('/getUserInfo', controller.user.getUserInfo);
   // 编辑用户信息
   router.post('/editUserInfo', controller.user.editUserInfo);
   // 获得全部用户信息
-  router.get('/getAllUserInfo', controller.user.getAllUserInfo)
+  router.get('/getAllUserInfo', controller.user.getAllUserInfo);
   // 获得全部管理员信息
-  router.get('/getAllManagerInfo', controller.user.getAllManagerInfo)
+  router.get('/getAllManagerInfo', controller.user.getAllManagerInfo);
 };
